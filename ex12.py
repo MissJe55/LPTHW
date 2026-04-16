080b60f08574d0120f6ed956bf784918e937d5d9
MissJe55 <117053891+MissJe55@users.noreply.github.com>
2026-04-16 18:39:09 -0400

    Easier Way To Prompt

---
+++ ex12.py
@@ -0,0 +1,8 @@
+#Ex 12 An easier way to prompt
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} years old, {height} inches tall, and {weight} pounds of pure love.")
+
