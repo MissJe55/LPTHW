788d8bca8898bb1b0646657292a3936dd61143f4
MissJe55 <117053891+MissJe55@users.noreply.github.com>
2026-04-16 18:38:45 -0400

    Asking People Questions
    Prompts and Input practice

--- Ex11.py
+++ Ex11.py
@@ -1,6 +1,6 @@
 #Ex 11 Asking People Questions
 
-print("How old are you?", end='')
+print("How old are you?")
 age = input()
 
 print("How tall are you?", end='')
@@ -10,3 +10,9 @@ print("How much do you weigh?", end='')
 weight = input()
 
 print(f"So, you're {age} years old, {height} inches tall, and {weight} pounds of pure love.")
+
+#Playing with print statements
+
+print("A", end= '_')
+print("B", end= '_')
+print("C")
