7c5c2befc41c1f989bed2db8860f067939c267e9
MissJe55 <117053891+MissJe55@users.noreply.github.com>
2026-04-22 19:49:55 -0400

    Prompting and Passing
    Added additional argv and input samples

--- ex14.py
+++ ex14.py
@@ -1,12 +1,12 @@
 #Ex 14 - Prompting and Passing
 #Using argv and input together to ask the user something specific
 
-from sys import argv
+from sys import argv  #argument vector - accepts input at the moment program is run
 
-script, user_name = argv
+script, user_name, user_age, favorite_food = argv #declares items to be used with argv
 prompt = '> '
 
-print(f"Hi {user_name}, I'm the {script} script.")
+print(f"Hi {user_name}, I'm the {script} script, congrats on reaching age {user_age} ")
 print("I'd like to ask you a few questions.")
 print(f"Do you like me {user_name}?")
 likes = input(prompt)
@@ -14,10 +14,13 @@ likes = input(prompt)
 print(f"Where do you live {user_name}?")
 lives = input(prompt)
 
+print("What is your favorite food?")
+fave = input(prompt)
+
 print("What kind of computer do you have?")
 computer = input(prompt)
 
 print(f'''Alright, so you said {likes} about liking me.
-You live in {lives}. Not sure where that is.
+You live in {lives}. Not sure where that is. Your fave food is {favorite_food} ,
 And you have a {computer} computer. Nice.''')
 
