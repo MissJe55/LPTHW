1232e2b869793e64560471c5bb8751dba09a136c
MissJe55 <117053891+MissJe55@users.noreply.github.com>
2026-06-24 11:35:02 -0400

    Dictionaries and Modules

---
+++ exercises/ex26.py
@@ -0,0 +1,21 @@
+#Ex 26 - Dictionaries and Modules
+#Exploring how dict works with modules
+
+#Let's review how import works
+
+name = "Jessica"
+age = 32
+height = 55
+
+# Importing the ex26.py program into REPL to further understand import modules
+
+#>>> import ex26 
+#>> print("name", ex26.name)
+#name Jessica
+#>>> print("age", ex26.age)
+#age 32
+#>>> print("height", ex26.height)
+
+#from pprint import pprint   //allowed us to print the __dict__ that contains everything int he module
+#pprint(ex26.__dict__)       //^^
+
