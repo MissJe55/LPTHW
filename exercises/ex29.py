3c61c48cd6020c10533b15582ee3ddbbb71e3a86
MissJe55 <117053891+MissJe55@users.noreply.github.com>
2026-06-29 21:39:09 -0400

    ex 29.py
    Boolean Logic / Practice

---
+++ exercises/ex29.py
@@ -0,0 +1,51 @@
+#Ex 29 - Boolean Practice
+#Answer the Boolean Practice questions and check your answers after typing them
+
+print(True and True)
+print(False and True)
+print(1 == 1 and 2 == 1)
+print("test" == "test")
+print(1 == 1 or 2 != 1)
+print(True and 1 == 1)
+print(False and 0 != 0)
+print("True" or 1 == 1)
+print("test" == "testing")
+print(1 != 0 and 2 == 1)
+print("test" != "testing")
+print("test" == 1)
+print(not (True and False))
+print(not (1 == 1 and 0 != 1))
+print(not ( 10 == 1 or 1000 == 1000))
+print(1 != 10 or 3 == 4)
+print(not("testing" == "testing" and "Jess" == "Cool Gal"))
+print(1 == 1 and (not ("testing" == 1 or 1 == 0)))
+print("chunky" == "bacon" and (not (3 == 4 or 3 == 3)))
+print(3 != 3 and (not ("testing" == "testing" or "Python" == "Fun")))
+                       
+#Let's check this against my answers in the book
+# I got 16 and 17 wrong, let's find out why
+
+#print(1 != 10 or 3 == 4) = True or False (One True make entire statement true)  I marked it false
+#print(not("testing" == "testing" and "Zed" == "Cool Guy")) True and False (I dont if Zed is a cool guy) I marked it false 
+#18/20 on Boolean Practice
+
+True
+False
+False
+True
+True
+True
+False
+True
+False
+False
+True
+False
+True
+False
+False
+True
+True
+True
+False
+False   
