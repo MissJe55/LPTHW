a0be1276bfac08563eaa36a224b7b22d46ab8111
MissJe55 <117053891+MissJe55@users.noreply.github.com>
2026-08-13 13:05:59 -0400

    Doing Things to Dictionaries
    Example of a dictitonary

---
+++ exercises/ex43.py
@@ -0,0 +1,72 @@
+#Ex 43 - Doing Things to Dictionaries
+#Create a mapping of state to abbreviation
+
+states = {
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'Virginia': 'VA',
+    'Florida': 'FL',
+    'Rhode Island': 'RI'
+}
+
+#Create a basic set of states and some cities in them
+
+cities = {
+    'RI': 'S Falls',
+    'NC': 'Raleigh',
+    'VA': 'Richmond',
+    'FL': 'Jacksonville'
+
+}
+
+#Add some more cities
+cities['NY'] = 'New York'
+cities['FL'] = 'Tallahasee'
+
+#Print out some cities
+print('-' * 10)
+print("NY State has: ", cities['NY'])
+print("FL State has: ", cities['FL'])
+
+#Print some states
+print('-' * 10)
+print("Virginia's abbreviation is: ", states['Virginia'])
+print("Florida's abbreviation is: ", states['Florida'])
+
+#Do it by using the state then cities dict
+print('-' * 10)
+print("North Carolina has: ", cities[states['North Carolina']])
+print("Florida has: ", cities[states['Florida']])
+
+#Print every state abbreviation
+print('-' * 10)
+for state, abbrev in list(states.items()):
+    print(f"{state} has the city {abbrev}")
+
+#Print every city in the state
+print('-' * 10)
+for abbrev, city in list (cities.items()):
+    print(f"{abbrev} has the city {city}")
+
+#Now do the same for both
+print('-' * 10)
+for state, abbrev in list(states.items()):
+    print(f"{state} state is abbreviated {abbrev}")
+    print(f" and has city {cities[abbrev]}")
+
+print('-' * 10)
+#Safely get an abbreviation by state that might not be there
+state = states.get('Texas')
+
+if not state:
+    print("Sorry, no Texas")
+
+#Get a city with a default value
+city = cities.get('TX', 'Does Not Exist')
+print(f"The city for state 'TX' is: {city}")
+
+
+          
+
+
+          
